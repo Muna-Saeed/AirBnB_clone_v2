e5f4c5142015b615ba65bca67f520ccf14a322b5
Muna Saeed <munasaeed6367@gmail.com>
2024-02-05 16:10:48 -0800

    done

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -8,9 +8,9 @@ from fabric.api import *
 from os.path import exists
 from datetime import datetime
 
-env.hosts = ['35.153.98.151', '54.146.56.220']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/school'
+env.hosts = ["35.153.98.151", "54.146.56.220"]
+env.user = "ubuntu"
+env.key_filename = "~/.ssh/school"
 
 
 def do_pack():
@@ -18,11 +18,15 @@ def do_pack():
     Creates a compressed archive of the web_static folder
     """
     try:
-        timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-        local('mkdir -p versions')
-        local('tar -czvf versions/web_static_{}.tgz web_static'.format(timestamp))
-        return 'versions/web_static_{}.tgz'.format(timestamp)
-    except:
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        local("mkdir -p versions")
+        local(
+                "tar -czvf versions/web_static_{}.tgz web_static".format(
+                    timestamp
+                    )
+                )
+        return "versions/web_static_{}.tgz".format(timestamp)
+    except Exception as e:
         return None
 
 
@@ -39,20 +43,30 @@ def do_deploy(archive_path):
 
         put(archive_path, "/tmp/{}".format(archive_filename))
         run("mkdir -p /data/web_static/releases/{}/".format(archive_no_ext))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(archive_filename, archive_no_ext))
+        run(
+            "tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
+                archive_filename, archive_no_ext
+            )
+        )
         run("rm /tmp/{}".format(archive_filename))
-        run("mv /data/web_static/releases/{}/web_static/* "
-            "/data/web_static/releases/{}/"
-            .format(archive_no_ext, archive_no_ext))
-        run("rm -rf /data/web_static/releases/{}/web_static"
-            .format(archive_no_ext))
+        run(
+            "mv /data/web_static/releases/{}/web_static/* "
+            "/data/web_static/releases/{}/".format(
+                archive_no_ext, archive_no_ext
+                )
+        )
+        run(
+                "rm -rf /data/web_static/releases/{}/web_static".format(
+                    archive_no_ext
+                    )
+                )
         run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ "
-            "/data/web_static/current"
-            .format(archive_no_ext))
+        run(
+            "ln -s /data/web_static/releases/{}/ "
+            "/data/web_static/current".format(archive_no_ext)
+        )
         return True
-    except:
+    except Exception as e:
         return False
 
 
